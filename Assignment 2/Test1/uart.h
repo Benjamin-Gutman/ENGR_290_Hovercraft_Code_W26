657f55887c50d24dace63ad64f66058d6a053160
Benjamin Gutman <benjamingutman@icloud.com>
2026-03-15 17:47:31 -0400

    changed uart.h

--- Assignment 2/Test1/uart.h
+++ Assignment 2/Test1/uart.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void UartInitialize(void);
 void UartPrintCharacter(char c);
 void UartPrintString(const char *s);
@@ -10,5 +14,7 @@ void UartAddNewLine(void);
 void UartPrint_u16(uint16_t value);
 void UartPrint_i16(int16_t value);
 void UartPrint_float(float value);
-
+#ifdef __cplusplus
+}
+#endif
 #endif
