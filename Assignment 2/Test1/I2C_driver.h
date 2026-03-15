b12385c5656da025047e55eb5af1ba56051e40c6
SarkisSeray <sarkisseraydarian@gmail.com>
2026-03-15 14:08:25 -0400

    Create I2C_driver.h

---
+++ Assignment 2/Test1/I2C_driver.h
@@ -0,0 +1,18 @@
+#ifndef I2C_DRIVER_H
+#define I2C_DRIVER_H
+
+#include <stdint.h>
+
+#define MPU_ADDR 0x68
+
+void I2C_init(void);
+void I2C_start(void);
+void I2C_stop(void);
+void I2C_write(uint8_t Data);
+void I2C_write_reg(uint8_t Data, uint8_t Register);
+uint8_t I2C_read_ack(void);
+uint8_t I2C_read_nack(void);
+uint8_t I2C_read_reg(uint8_t Register);
+void I2C_read_multiple_reg(uint8_t Base_reg, uint8_t* values, int range);
+
+#endif
