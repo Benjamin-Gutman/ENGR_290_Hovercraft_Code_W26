a3f8ddf94bcd8ff058f4504091c653fc02685505
SarkisSeray <sarkisseraydarian@gmail.com>
2026-03-15 15:00:24 -0400

    Update uart.h

--- Assignment 2/Test1/uart.h
+++ Assignment 2/Test1/uart.h
@@ -8,5 +8,7 @@ void UartPrintCharacter(char c);
 void UartPrintString(const char *s);
 void UartAddNewLine(void);
 void UartPrint_u16(uint16_t value);
+void UartPrint_i16(int16_t value);
+void UartPrint_float(float value);
 
 #endif
