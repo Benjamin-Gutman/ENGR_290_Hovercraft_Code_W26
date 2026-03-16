237f8ac3e02e0cea69b9fef6a870aae909914d30
SarkisSeray <sarkisseraydarian@gmail.com>
2026-03-15 20:34:02 -0400

    Update L_led.h

--- Assignment 2/TestOriginal/L_led.h
+++ Assignment 2/TestOriginal/L_led.h
@@ -1,1 +1,12 @@
+#ifndef IMU_LED_H
+#define IMU_LED_H
 
+#include <stdint.h>
+
+// Limits for allowed yaw range
+#define YAW_LIMIT 85.0
+
+// Function to update LED based on yaw
+void imu_led_update(float yaw);
+
+#endif
