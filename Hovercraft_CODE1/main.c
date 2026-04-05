82c86730bff4c9462df1da3c5cd806f74211fa69
pecorpa <pecorpa@aol.com>
2026-04-04 23:15:47 -0400

    Update main.c

--- Hovercraft_CODE1/main.c
+++ Hovercraft_CODE1/main.c
@@ -1,1 +1,44 @@
+ sensors_init();
+    battery_init();
 
+    _delay_ms(300);
+
+    /* First sensor update before setting the initial heading target */
+    update_sensors();
+    battery_update();
+
+    yaw_target = yaw_deg;
+
+    /* Start lift fan */
+    set_lift(LIFT_CRUISE_PERCENT);
+
+    while (1)
+    {
+        update_sensors();
+
+        /* Battery does not need to be updated every loop */
+        battery_loop_divider++;
+        if (battery_loop_divider >= 20) {
+            battery_loop_divider = 0;
+            battery_update();
+        }
+
+        /* Optional simple low-battery failsafe */
+        if (battery_is_low()) {
+            set_thrust(0);
+            set_lift(0);
+            set_servo_angle(0.0f);
+
+            while (1) {
+                _delay_ms(100);
+            }
+        }
+
+        update_state();
+        apply_control();
+
+        _delay_ms(10);
+    }
+
+    return 0;
+}
