6821223c12aefd18eeda33425c64877b6c9b846a
pecorpa <pecorpa@aol.com>
2026-04-04 23:13:28 -0400

    Create thrust.h

---
+++ Hovercraft_CODE1/thrust.h
@@ -0,0 +1,15 @@
+#ifndef THRUST_H
+#define THRUST_H
+
+#include <stdint.h>
+
+/* Initialize thrust fan PWM output */
+void thrust_init(void);
+
+/* Set thrust fan command in percent: 0..100 */
+void set_thrust(uint8_t percent);
+
+/* Return last commanded thrust percentage */
+uint8_t get_thrust(void);
+
+#endif
