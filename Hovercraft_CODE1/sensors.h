8cac3cfea0dc77820e700747678aa7d7d3579b4d
pecorpa <pecorpa@aol.com>
2026-04-07 23:04:23 -0400

    Update sensors.h

--- Hovercraft_CODE1/sensors.h
+++ Hovercraft_CODE1/sensors.h
@@ -4,28 +4,27 @@
 #include <stdint.h>
 #include "IMU.h"
 
-/* Global IR distances in centimeters */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern float left_cm;
 extern float front_cm;
 
-/* Raw ADC values for debugging and calibration */
 extern uint16_t left_adc;
 extern uint16_t front_adc;
 
-/* IMU outputs used by control and state logic */
 extern float yaw_deg;
 extern float yaw_rate_dps;
 
-/* Full IMU state */
 extern imu_state_t imu;
 
-/* Initialize ADC + I2C + IMU */
 void sensors_init(void);
-
-/* Read all sensors and update global variables */
 void update_sensors(void);
-
-/* Helper to check if an IR reading is inside the useful range */
 uint8_t ir_is_valid(float cm);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
